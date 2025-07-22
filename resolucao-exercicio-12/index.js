94395d5aef18a088eda95e32ccca2eeb85d50f1a
Iago Pinheiro <iagoo.pinheiro73@gmail.com>
2025-07-21 22:29:19 -0300

    Manipulando estilos com DOM e Correção exercício 12

--- resolucao-exercicio-12/index.js
+++ resolucao-exercicio-12/index.js
@@ -14,3 +14,70 @@ function createInput(id, value, name, type = 'text', placeholder = ''){
   input.placeholder = placeholder
   return input
 }
+
+const addTechBtn = document.getElementById('addTechBtn')
+const form = document.getElementById('devForm')
+const developers = []
+let inputRows = 0
+
+addTechBtn.addEventListener('click', function (ev) {
+  const stackInputs = document.getElementById('stackInputs')
+
+  const newRow = document.createElement('li')
+  const rowIndex = inputRows
+  inputRows++
+  newRow.id = 'inputRow-' + rowIndex // pra cada linha criada vai ter um id diferente tipo um auto_increment
+  newRow.className = 'inputRow'
+
+  const techNameLabel = createLabel('Nome: ', 'techname-' + rowIndex)
+  const techNameInput = createInput('techName-' + rowIndex, null, 'techName')
+
+  const expLabel = createLabel('Experiência: ')
+  const id1 = 'expRadio-' + rowIndex + '.1'
+  const expRadio1 = createInput(id1, '0-2 anos', 'techExp-' + rowIndex, 'radio')
+  const expLabel1 = createLabel('0-2 anos', id1)
+  const id2 = 'expRadio-' + rowIndex + '.2'
+  const expRadio2 = createInput(id2, '3-4 anos', 'techExp-' + rowIndex, 'radio')
+  const expLabel2 = createLabel('3-4 anos', id2)
+  const id3 = 'expRadio-' + rowIndex + '.3'
+  const expRadio3 = createInput(id3, '5+ anos', 'techExp-' + rowIndex, 'radio')
+  const expLabel3 = createLabel('5+ anos', id3)
+
+  const removeRowBtn = document.createElement('button')
+  removeRowBtn.type = 'button'
+  removeRowBtn.innerText = 'Remover'
+  removeRowBtn.addEventListener('click', function (){
+    stackInputs.removeChild(newRow)
+  })
+
+  newRow.append(
+    techNameLabel, techNameInput, expLabel, expRadio1, expLabel1, expRadio2, expLabel2, expRadio3, expLabel3, removeRowBtn
+  )
+
+  stackInputs.appendChild(newRow)
+})
+
+form.addEventListener('submit', function (ev){
+  ev.preventDefault()
+
+  const fullNameInput = document.getElementById('fullname')
+  const inputRows = document.querySelectorAll('.inputRow')
+
+  let technologies = []
+  inputRows.forEach(function (row){
+    const techName = document.querySelector('#' + row.id + ' input[name="techName"]').value
+    const techExp = document.querySelector('#' + row.id + ' input[type="radio"]:checked').value
+    technologies.push({ name: techName, exp: techExp })
+  })
+
+  const newDev = { fullname: fullNameInput.value, technologies: technologies}
+  developers.push(newDev)
+  alert('Dev cadastrado com sucesso!')
+
+  fullNameInput.value = ''
+  inputRows.forEach(function (row){
+    row.remove()
+  })
+
+  console.log(developers)
+})
